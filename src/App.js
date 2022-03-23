4c9759ea8621d1e07b1df941cf80c1cc96d1886a
Lucas <lucas.ar93@gmail.com>
2022-03-23 10:29:41 -0300

    turn get pokemon functions asynchronous

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import PokemonThumbNail from './components/PokemonThumbNail';
-import Button from '@material-ui/core/Button';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonThumbNail from "./components/PokemonThumbNail";
+import "./App.css";
 
 function App() {
-  const [allPokemons, setAllPokemons] = useState([])
-  const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon?limit=20")
+  const [pokemons, setPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=20"
+  );
 
   const getPokemons = async () => {
-    const resp = await fetch(loadMore)
-    const data = await resp.json()
+    const resp = await fetch(loadMore);
+    const data = await resp.json();
 
-    setLoadMore(data.next)
-    
-    function createPokemonObject (result) {
+    setLoadMore(data.next);
+
+    const createPokemonObject = async (result) => {
       result.forEach(async (pokemon) => {
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+        const resp = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
         const data = await resp.json();
-     
-        setAllPokemons(currentList => [...currentList, data]);
-     });
-    }
-      createPokemonObject(data.results)
+        await setPokemons((currentList) => [...currentList, data]);
+      });
+    };
+    createPokemonObject(data.results);
   };
-
-  useEffect(() =>{
-    getPokemons()
-  }, [])
+  useEffect(() => {
+    getPokemons();
+  }, []);
 
   return (
     <div className="app-container">
       <h1 className="title">Pokédex</h1>
       <div className="pokemon-container">
         <div className="all-container">
-          {allPokemons.sort((a, b) => a.id > b.id? 1 : -1).map((pokemon, index) => 
-            <PokemonThumbNail 
-              id={pokemon.id}
-              name={pokemon.name}
-              image={pokemon.sprites.other.dream_world.front_default}
-              type={pokemon.types[0].type.name}
-              key={index}
-            />
-          )}
+          {pokemons
+            .sort((a, b) => (a.id > b.id ? 1 : -1))
+            .map((pokemon, id) => {
+              return (
+                <PokemonThumbNail
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  image={pokemon.sprites.other.dream_world.front_default}
+                  type={pokemon.types[0].type.name}
+                  key={id}
+                />
+              );
+            })}
         </div>
-        <Button onClick={() => getPokemons()} className="load-more">
-          Load more
-        </Button>
       </div>
+      <button onClick={() => getPokemons()} type="button">
+        Load more
+      </button>
     </div>
   );
 }
--- src/components/PokemonThumbNail.js
+++ src/components/PokemonThumbNail.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const pokemonThumbNail = ({id, name, image, type}) => {
-  const style = `thumb-container ${type}`
+const pokemonThumbNail = ({ id, name, image, type }) => {
+  const style = `thumb-container ${type}`;
   return (
     <div className={style}>
       <div className="number">
@@ -9,11 +9,11 @@ const pokemonThumbNail = ({id, name, image, type}) => {
       </div>
       <img src={image} alt={name} />
       <div className="detail-wrapper">
-        <h3>{name.charAt(0).toUpperCase() + name.slice(1)}</h3>
+        <h3>{name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}</h3>
         <h3>Type: {type}</h3>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default pokemonThumbNail
+export default pokemonThumbNail;
